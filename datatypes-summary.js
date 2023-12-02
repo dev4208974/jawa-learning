ae32e0640a44d117bc24695c51733958dc673c77
rahul sahani <150283720+dev4208974@users.noreply.github.com>
2023-12-02 11:55:28 +0000

    datatype

---
+++ datatypes-summary.js
@@ -0,0 +1,34 @@
+//primitive
+
+//  types : string ,number , Boolearn , null , undefined , Symbol , BigInt ,
+
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false 
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+//const bigNumber = 345673545868446383n
+
+// Reference (NoN primitive)
+
+// Array, Objets, Functions 
+
+const heros  = ["shaktiman", "naagraj","doga"]
+{
+    name: "rahul",
+    age: 22
+}
+const myFunction = function(){
+    console.log("Hello world");
+
+}
+
+console.log(typeof bigNumber);
